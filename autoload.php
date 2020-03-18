a15f094f45d0816cea1bba6dd4b5a3d422ee145d
Cyril Jacquesson <cyril@jacquesson.me>
2020-03-18 17:04:33 +0100

    Externalize main class

--- autoload.php
+++ autoload.php
@@ -9,9 +9,6 @@ spl_autoload_register(
 	function ( $name ) {
 		$sanitized_basename = str_replace( '_', '-', sanitize_title( basename( str_replace( '\\', '/', $name ) ) ) );
 
-		if ( strpos( $name, 'BMFBE\\Plugin' ) === 0 ) {
-			require_once __DIR__ . '/plugin.php';
-		}
 		if ( strpos( $name, 'BMFBE\\' ) === 0 ) {
 			require_once __DIR__ . '/includes/class-' . $sanitized_basename . '.php';
 		}
