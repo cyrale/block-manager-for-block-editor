27d83b8d8d58c13055ffb50a055ca752e22a3929
Cyril Jacquesson <cyril@jacquesson.me>
2020-04-23 18:39:53 +0200

    Basic variations component

--- src/libs/admin/components/block/variations.js
+++ src/libs/admin/components/block/variations.js
@@ -1,5 +1,41 @@
-const Variations = () => (
-	<div className="bmfbe-block__variations">variations</div>
+import { useState } from 'react';
+
+const Variation = ( props ) => {
+	const [ isActive, setIsActive ] = useState( props.isActive );
+	const [ isDefault, setIsDefault ] = useState( props.isDefault );
+
+	const handleIsActiveChange = () => {
+		setIsActive( ! isActive );
+	};
+
+	const handleIsDefaultChange = () => {
+		setIsDefault( ! isDefault );
+	};
+
+	return (
+		<div className="bmfbe-block__variation">
+			<input
+				type="checkbox"
+				checked={ isActive }
+				onChange={ handleIsActiveChange }
+			/>
+			{ props.title }
+			<em>{ props.name }</em>
+			<input
+				type="checkbox"
+				checked={ isDefault }
+				onChange={ handleIsDefaultChange }
+			/>
+		</div>
+	);
+};
+
+const Variations = ( { variations } ) => (
+	<div className="bmfbe-block__variations">
+		{ variations.map( ( variation ) => (
+			<Variation key={ variation.name } { ...variation } />
+		) ) }
+	</div>
 );
 
 export default Variations;
