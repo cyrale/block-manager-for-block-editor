5c7bb99d77c675f685ecc12e29723743ddd728da
Cyril Jacquesson <cyril@jacquesson.me>
2020-04-23 16:28:47 +0200

    Complete icon component

--- src/libs/admin/block-icon.js
+++ src/libs/admin/block-icon.js
@@ -1,5 +1,17 @@
-const BlockIcon = ( props ) => (
-	<div className="bmfbe-block__icon">{ props.icon }</div>
-);
+const BlockIcon = ( props ) => {
+	if ( 'string' === typeof props.icon && '' !== props.icon ) {
+		if ( ( /^<svg (.*)<\/svg>$/i ).test( props.icon ) ) {
+			return (
+				<div className="bmfbe-block__icon" dangerouslySetInnerHTML={ { __html: props.icon } }/>
+			);
+		}
+
+		return (
+			<div className="bmfbe-block__icon"><span className={ `dashicons dashicons-${ props.icon }` }/></div>
+		);
+	}
+
+	return <></>;
+}
 
 export default BlockIcon;
