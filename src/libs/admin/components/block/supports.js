dcb2298ceb2f022cb2f9dd3939e9a293006a1bc1
Cyril Jacquesson <cyril@jacquesson.me>
2020-04-23 18:25:11 +0200

    Merge item and items list

--- src/libs/admin/components/block/supports.js
+++ src/libs/admin/components/block/supports.js
@@ -1,8 +1,38 @@
-import Support from '../support';
+import { useState } from 'react';
 
-const Supports = ( props ) => (
+const Support = ( props ) => {
+	const [ isActive, setIsActive ] = useState( props.isActive );
+	const [ value, setValue ] = useState( props.value );
+
+	const handleIsActiveChange = () => {
+		setIsActive( ! isActive );
+	};
+
+	const handleValueChange = () => {
+		setValue( ! value );
+	};
+
+	return (
+		<div className="bmfbe-block__support">
+			<input
+				type="checkbox"
+				checked={ isActive }
+				onChange={ handleIsActiveChange }
+			/>
+			{ props.name }
+			<input
+				type="checkbox"
+				checked={ value }
+				onChange={ handleValueChange }
+				disabled={ ! isActive }
+			/>
+		</div>
+	);
+};
+
+const Supports = ( { supports } ) => (
 	<div className="bmfbe-block__supports">
-		{ Object.entries( props ).map( ( [ name, args ] ) => (
+		{ Object.entries( supports ).map( ( [ name, args ] ) => (
 			<Support key={ name } { ...Object.assign( args, { name } ) } />
 		) ) }
 	</div>
