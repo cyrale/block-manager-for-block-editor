555f274f22b5684d33d78c752d4e9f1e4bfe291a
Cyril Jacquesson <cyril@jacquesson.me>
2020-04-23 18:25:46 +0200

    Basic styles component

--- src/libs/admin/components/block/styles.js
+++ src/libs/admin/components/block/styles.js
@@ -1,3 +1,41 @@
-const Styles = () => <div className="bmfbe-block__styles">styles</div>;
+import { useState } from 'react';
+
+const Style = ( props ) => {
+	const [ isActive, setIsActive ] = useState( props.isActive );
+	const [ isDefault, setIsDefault ] = useState( props.isDefault );
+
+	const handleIsActiveChange = () => {
+		setIsActive( ! isActive );
+	};
+
+	const handleIsDefaultChange = () => {
+		setIsDefault( ! isDefault );
+	};
+
+	return (
+		<div className="bmfbe-block__style">
+			<input
+				type="checkbox"
+				checked={ isActive }
+				onChange={ handleIsActiveChange }
+			/>
+			{ props.label }
+			<em>{ props.name }</em>
+			<input
+				type="checkbox"
+				checked={ isDefault }
+				onChange={ handleIsDefaultChange }
+			/>
+		</div>
+	);
+};
+
+const Styles = ( { styles } ) => (
+	<div className="bmfbe-block__styles">
+		{ styles.map( ( style ) => (
+			<Style key={ style.name } { ...style } />
+		) ) }
+	</div>
+);
 
 export default Styles;
