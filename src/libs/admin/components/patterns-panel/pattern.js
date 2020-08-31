bb88d86c423888a2ea0fe89a52a228e39131850a
Cyril Jacquesson <cyril@jacquesson.me>
2020-08-31 15:05:21 +0200

    Checkbox to disable patterns

--- src/libs/admin/components/patterns-panel/pattern.js
+++ src/libs/admin/components/patterns-panel/pattern.js
@@ -1,9 +1,33 @@
 /**
+ * WordPress dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies.
  */
 import Description from '../description';
+import IndeterminateToggleControl from '../indeterminate-toggle-control';
 
-export default function Pattern( { description, name, title } ) {
+export default function Pattern( {
+	description,
+	disabled: patternDisabled = false,
+	name,
+	title,
+} ) {
+	/**
+	 * Handle changes on pattern disabled.
+	 *
+	 * @param {boolean} disabled TRUE if patter is disabled.
+	 * @since 1.0.0
+	 */
+	async function handleDisabledChange( disabled ) {
+		console.log( disabled ); // eslint-disable-line no-console
+		// await updateBlock( blockName, value );
+		// const newBlock = wpSelect( BLOCKS_STORE ).getBlock( blockName );
+		// enqueueChanges( pick( newBlock, changingFields ) );
+	}
+
 	return (
 		<div className="bmfbe-pattern">
 			{ /* <StatusIcon status={ status } /> */ }
@@ -15,16 +39,12 @@ export default function Pattern( { description, name, title } ) {
 					description={ description }
 				/>
 			</div>
-			{ /* <IndeterminateToggleControl
-				label={ __( 'Override supports?', 'bmfbe' ) }
-				className="bmfbe-block__override-supports"
-				checked={ block.supports_override }
-				onChange={ ( { checked } ) =>
-					handleBlockChange( {
-						supports_override: checked,
-					} )
-				}
-			/> */ }
+			<IndeterminateToggleControl
+				label={ __( 'Disable pattern', 'bmfbe' ) }
+				className="bmfbe-pattern__disable"
+				checked={ patternDisabled }
+				onChange={ ( { checked } ) => handleDisabledChange( checked ) }
+			/>
 		</div>
 	);
 }
