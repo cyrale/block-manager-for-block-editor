9062fd709f826d5748213c303b848e1eb0b00867
Cyril Jacquesson <cyril@jacquesson.me>
2020-09-15 16:06:45 +0200

    Refactoring

--- src/stores/blocks/controls.js
+++ src/stores/blocks/controls.js
@@ -17,40 +17,26 @@ import {
 export const API_FETCH_ALL_CATEGORIES = allBlockCategories;
 export const API_FETCH_ALL = allBlocks;
 
-export function CREATE_ITEMS( { items } ) {
+function batchProcess( items, cb ) {
 	return chunk( items, 10 ).reduce(
 		async ( memo, part ) => [
 			...( await memo ),
-			...( await Promise.all(
-				part.map( ( item ) => createBlock( item ) )
-			) ),
+			...( await Promise.all( part.map( ( item ) => cb( item ) ) ) ),
 		],
 		[]
 	);
+}
+
+export function CREATE_ITEMS( { items } ) {
+	return batchProcess( items, createBlock );
 }
 
 export function UPDATE_ITEMS( { items } ) {
-	return chunk( items, 10 ).reduce(
-		async ( memo, part ) => [
-			...( await memo ),
-			...( await Promise.all(
-				part.map( ( item ) => updateBlock( item ) )
-			) ),
-		],
-		[]
-	);
+	return batchProcess( items, updateBlock );
 }
 
 export function DELETE_ITEMS( { items } ) {
-	return chunk( items, 10 ).reduce(
-		async ( memo, part ) => [
-			...( await memo ),
-			...( await Promise.all(
-				part.map( ( { name } ) => deleteBlock( name ) )
-			) ),
-		],
-		[]
-	);
+	return batchProcess( items, deleteBlock );
 }
 
 export function SAVE_ITEM( { item } ) {
