b364129b51fee94c8f0bb446a787bc6c4bca5acb
Cyril Jacquesson <cyril@jacquesson.me>
2020-09-18 11:37:08 +0200

    Simplify condition

--- src/admin-page/components/blocks-panel/icon.js
+++ src/admin-page/components/blocks-panel/icon.js
@@ -6,7 +6,7 @@ import { Icon as WPIcon } from '@wordpress/components';
 export default function Icon( { icon } ) {
 	return (
 		<div className="bmfbe-block__icon">
-			{ icon && icon.src && <WPIcon icon={ icon.src } /> }
+			{ icon?.src && <WPIcon icon={ icon.src } /> }
 		</div>
 	);
 }
